e03fd25873880907aecfbfee2fdab585a633e654
vuphong0x <vuphong0x@gmail.com>
2021-02-19 07:50:32 +0700

    Init

---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const dacNhanTam = require("../assets/images/dac_nhan_tam.jpg");
+export const theMetropolist = require("../assets/images/the_metropolist.jpg");
+export const theTinyDragon = require("../assets/images/the_tiny_dragon.jpg");
+export const underland = require("../assets/images/underland.jpg");
+
+export default {
+    dacNhanTam,
+    theMetropolist,
+    theTinyDragon,
+    underland
+}
